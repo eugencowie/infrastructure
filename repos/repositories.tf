1652429c68c29163982e74b8d8d2a7f6af8eb03f
Eugén Cowie <eugencowie@users.noreply.github.com>
2026-08-30 16:46:28 +0100

    Configure repository merge settings
    - Disable project boards and rebase/squash merges
    - Enable auto-merge and delete branches after merging

--- repos/repositories.tf
+++ repos/repositories.tf
@@ -8,11 +8,18 @@ locals {
 }
 
 resource "github_repository" "this" {
-  for_each     = local.repositories
-  name         = each.key
-  description  = each.value.description
-  visibility   = each.value.visibility
+  for_each = local.repositories
+
+  name        = each.key
+  description = each.value.description
+  visibility  = each.value.visibility
+
   has_issues   = true
-  has_projects = true
   has_wiki     = true
+  has_projects = false
+
+  allow_rebase_merge     = false
+  allow_squash_merge     = false
+  allow_auto_merge       = true
+  delete_branch_on_merge = true
 }
