9d1a23dc2413a8548ee8f2169ff0bad584b18425
Eugén Cowie <eugencowie@users.noreply.github.com>
2026-08-30 18:18:19 +0100

    Add standard issue labels
    - Configure shared labels for all repositories

---
+++ repos/labels.tf
@@ -0,0 +1,70 @@
+import {
+  for_each = local.repositories
+  to       = github_issue_labels.this[each.key]
+  id       = each.key
+}
+
+resource "github_issue_labels" "this" {
+  for_each   = local.repositories
+  repository = github_repository.this[each.key].id
+
+  label {
+    name        = "chore"
+    description = "Routine maintenance and housekeeping"
+    color       = "cfd3d7"
+  }
+
+  label {
+    name        = "ci"
+    description = "Changes to CI workflows or automation"
+    color       = "ffffff"
+  }
+
+  label {
+    name        = "docs"
+    description = "Improvements or additions to documentation"
+    color       = "0075ca"
+  }
+
+  label {
+    name        = "feat"
+    description = "New feature or request"
+    color       = "a2eeef"
+  }
+
+  label {
+    name        = "fix"
+    description = "Something isn't working"
+    color       = "d73a4a"
+  }
+
+  label {
+    name        = "perf"
+    description = "Improvements to performance or efficiency"
+    color       = "008672"
+  }
+
+  label {
+    name        = "refactor"
+    description = "Code changes that do not alter behaviour"
+    color       = "7057ff"
+  }
+
+  label {
+    name        = "security"
+    description = "Security fixes or improvements"
+    color       = "e4e669"
+  }
+
+  label {
+    name        = "style"
+    description = "Formatting or styling changes"
+    color       = "d876e3"
+  }
+
+  label {
+    name        = "test"
+    description = "Improvements or additions to tests"
+    color       = "bfd4f2"
+  }
+}
